302e12b133ea0e38d5e128ec676c942558c529aa
Emin Developer <emin.f.mammadov@gmail.com>
2022-01-24 13:46:01 -0800

    Add WebFlux in xab

---
+++ WebFluxXab/src/main/java/com/example/webfluxxab/config/GreetingRouter.java
@@ -0,0 +1,38 @@
+package com.example.webfluxxab.config;
+
+import com.example.webfluxxab.component.GreetingHandler;
+import com.example.webfluxxab.domain.Greeting;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.*;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+
+@Configuration(proxyBeanMethods = false)
+public class GreetingRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> route(GreetingHandler greetingHandler) {
+
+        RequestPredicate route = GET("/hello")
+                .and(accept(MediaType.APPLICATION_JSON));
+
+
+        return RouterFunctions
+                .route(route, greetingHandler::hello)
+                .andRoute(
+                        GET("/"),
+                        serverRequest -> {
+                            return ServerResponse
+                                    .ok()
+                                    .contentType(MediaType.TEXT_PLAIN)
+                                    .body(
+                                            BodyInserters.fromValue("Main Page")
+                                    );
+                        }
+                );
+    }
+}
