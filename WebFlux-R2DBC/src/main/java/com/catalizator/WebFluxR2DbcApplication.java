f78452b0d83b07dec5699ef6665178036ad99ed5
Emin Developer <emin.f.mammadov@gmail.com>
2022-01-24 15:50:30 -0800

    new module r2dbc

---
+++ WebFlux-R2DBC/src/main/java/com/catalizator/WebFluxR2DbcApplication.java
@@ -0,0 +1,13 @@
+package com.catalizator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxR2DbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxR2DbcApplication.class, args);
+    }
+
+}
---
+++ WebFlux-R2DBC/src/test/java/com/catalizator/WebFluxR2DbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalizator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxR2DbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
