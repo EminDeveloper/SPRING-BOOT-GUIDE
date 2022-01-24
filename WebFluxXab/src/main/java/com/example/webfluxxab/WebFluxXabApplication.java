a050751d2b39897a65660e51d80fcf79a870b1d0
Emin Developer <emin.f.mammadov@gmail.com>
2022-01-24 08:53:37 -0800

    Add WebFlux in xab

---
+++ WebFluxXab/src/main/java/com/example/webfluxxab/WebFluxXabApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxxab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxXabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxXabApplication.class, args);
+    }
+
+}
---
+++ WebFluxXab/src/test/java/com/example/webfluxxab/WebFluxXabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluxxab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxXabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
