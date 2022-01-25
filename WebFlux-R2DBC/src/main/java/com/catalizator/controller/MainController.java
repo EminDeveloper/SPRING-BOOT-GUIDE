36e7b228759b8429f902aa97a4b2ed412a013c73
Emin Developer <emin.f.mammadov@gmail.com>
2022-01-24 16:42:41 -0800

    r2dbc add data in application.properties

--- WebFlux-R2DBC/src/main/java/com/catalizator/controller/MainController.java
+++ WebFlux-R2DBC/src/main/java/com/catalizator/controller/MainController.java
@@ -1,31 +1,32 @@
 package com.catalizator.controller;
 
 import com.catalizator.domain.Message;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.catalizator.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/controller")
 public class MainController {
 
-    /* http://localhost:8989/controller?start=1&count=2 */
+    private final MessageService messageService;
+
+    @Autowired
+    public MainController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
     @GetMapping
     public Flux<Message> list(
             @RequestParam(defaultValue = "0") Long start,
             @RequestParam(defaultValue = "3") Long count){
-        return Flux
-                .just(
-                        "Hello, reactive!",
-                        "More then one",
-                        "Third post",
-                        "Fourth post",
-                        "Fifth post"
-                )
-                .skip(start)
-                .take(count)
-                .map(Message::new);
+        return messageService.list();
+    }
+
+    @PostMapping
+    public Mono<Message> add(Message message){
+        return messageService.addOne(message);
     }
 }
