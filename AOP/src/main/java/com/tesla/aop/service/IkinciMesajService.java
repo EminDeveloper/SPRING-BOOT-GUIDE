82357bd61c04dc8e929fe58ba53b82545e969e83
Emin Developer <emin.f.mammadov@gmail.com>
2022-01-26 15:48:23 -0800

    add aop

---
+++ AOP/src/main/java/com/tesla/aop/service/IkinciMesajService.java
@@ -0,0 +1,12 @@
+package com.tesla.aop.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class IkinciMesajService {
+
+    public void mesaj(String param) {
+        System.out.println(param);
+    }
+}
