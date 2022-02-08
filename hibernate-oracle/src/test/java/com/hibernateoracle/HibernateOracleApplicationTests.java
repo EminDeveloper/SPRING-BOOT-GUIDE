658cd208ec3a67b2782d13db88f49a74ff340367
Emin Developer <emin.f.mammadov@gmail.com>
2022-02-07 16:57:19 -0800

    Hibernate add project

---
+++ hibernate-oracle/src/test/java/com/hibernateoracle/HibernateOracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernateoracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateOracleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
